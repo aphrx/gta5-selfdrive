9065342134ee0cafbcce18dc5b8f8ecc33878d6e
Aphrx <amalnnath20@yahoo.ca>
2019-09-13 09:54:32 -0400

    Add files via upload

--- gta5_auto.py
+++ gta5_auto.py
@@ -2,18 +2,98 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time
+import math
+from keyboard_mapper import W, A, S, D, Forward, Left, Right, Back, PressKey, ReleaseKey
 
 def process_img(img):
 	processed_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	## processed_img = cv2.GaussianBlur(processed_img, (5, 5), 0)
 	processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+	processed_img = cv2.GaussianBlur(processed_img, (5, 5), 0)
 	return processed_img
+
+def region_of_interest(img):
+	height = img.shape[0]
+	width = img.shape[1]
+	interest = np.array([[(0, 600),(0,400), (200, 300), (600, 300), (800, 400), (800, 600)]])
+	mask = np.zeros_like(img)
+	cv2.fillPoly(mask, interest, 255)
+	masked_img = cv2.bitwise_and(img, mask)
+	return masked_img
+
+def display_lines(img, lines):
+	line_image = np.zeros_like(img)
+	m1 = 0;
+	m2 = 0;
+	m1coords = [[0, 0], [0,0]]
+	m2coords = [[0, 0], [0,0]]
+
+	if lines is not None:
+		for line in lines:
+			x1, y1, x2, y2 = line.reshape(4)
+			
+			#x = ((x2.item()-x1.item())(x2.item()-x1.item()) + (y2.item()-y1.item())(y2.item()-y1.item()))
+			x = (float)(x2.item() - x1.item())
+			y = (float)(y2.item() - y1.item())
+			if (x != 0 and y != 0):
+				m = y/x
+				z = (x*x)+(y*y)
+				length = math.sqrt(z)
+				if(length > 0):
+					if(math.fabs(m) > 0.2 and math.fabs(m) < 0.6):
+						#length = math.sqrt((x*x)+(y*y))
+						if(m > 0 and m1 < length):
+							m1 = length
+							m1coords[0] = [x1, y1]
+							m1coords[1] = [x2, y2]
+							cv2.line(line_image, (m1coords[0][0], m1coords[0][1]), (m1coords[1][0], m1coords[1][1]), (255, 255, 255), 5)
+							
+						elif(m < 0 and m2 < length):
+							m2 = length
+							m2coords[0] = [x1, y1]
+							m2coords[1] = [x2, y2]
+							cv2.line(line_image, (m2coords[0][0], m2coords[0][1]), (m2coords[1][0], m2coords[1][1]), (255, 255, 255), 5)
+	
+	
+	
+	## cv2.line(line_image, (0, 400), (800, 400), (255, 255, 255), 5)		
+	return line_image, m1, m2
 
 def main():
 	while(True):
-		printscreen_pil = ImageGrab.grab(bbox=(0, 30, 1024, 768))
+		printscreen_pil = ImageGrab.grab(bbox=(0, 30, 800, 600))
 		img = cv2.cvtColor(np.array(printscreen_pil), cv2.COLOR_BGR2RGB)
-		cv2.imshow('window', process_img(img))
+		processed_img = region_of_interest(process_img(img))
+		
+		lines = cv2.HoughLinesP(processed_img, 2, np.pi/180, 100, np.array([]), minLineLength=100, maxLineGap=5)
+
+		lined_img, m1, m2 = display_lines(processed_img, lines)
+
+		overlayed_img = cv2.addWeighted(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 0.8, lined_img, 1, 1)
+
+		cv2.imshow('window', overlayed_img)
 		if cv2.waitKey(25) & 0xFF == ord('q'):
 			cv2.destroyAllWindows()
 			break
+		
+		start = 0
+		durTurn = 0
+
+		if(m1 == 0):
+			if(durTurn == 0):
+				start = time.time()
+				durTurn = 1
+				#PressKey(D)
+		elif(m2 == 0):
+			if(durTurn == 0):
+				start = time.time()
+				durTurn = 1
+				#PressKey(A)
+		if((time.time()-start==1) and durTurn == 1):
+			print("Releasing key D")
+			#ReleaseKey(D)
+			durTurn = 0
+		if((time.time()-start==1) and durTurn == 1):
+			print("Releasing key A")
+			#ReleaseKey(A)
+			durTurn = 0
+main()
--- keyboard_mapper.py
+++ keyboard_mapper.py
@@ -1,78 +1,79 @@
-## https://stackoverflow.com/questions/13564851/how-to-generate-keyboard-events-in-python
-## Contributed by Eryk Sun on Stack
+# https://stackoverflow.com/questions/14489013/simulate-python-keypresses-for-controlling-a-game
 
 import ctypes
-from ctypes import wintypes
 import time
 
-user32 = ctypes.WinDLL('user32', use_last_error=True)
+SendInput = ctypes.windll.user32.SendInput
 
-KEYBOARD = 1
+W = 0x11
+A = 0x1E  # A
+S = 0x1F  # S
+D = 0x20  # D
 
-KEYEVENTF_EXTENDEDKEY = 0x0001
-KEYEVENTF_KEYUP       = 0x0002
-KEYEVENTF_UNICODE     = 0x0004
-KEYEVENTF_SCANCODE    = 0x0008
 
-MAPVK_VK_TO_VSC = 0
+# C struct redefinitions 
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
 
-# msdn.microsoft.com/en-us/library/dd375731
-W       = 0x57 
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
 
-# C struct definitions
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
 
-wintypes.ULONG_PTR = wintypes.WPARAM
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
 
-class KEYBDINPUT(ctypes.Structure):
-    _fields_ = (("wVk",         wintypes.WORD),
-                ("wScan",       wintypes.WORD),
-                ("dwFlags",     wintypes.DWORD),
-                ("time",        wintypes.DWORD),
-                ("dwExtraInfo", wintypes.ULONG_PTR))
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
 
-    def __init__(self, *args, **kwds):
-        super(KEYBDINPUT, self).__init__(*args, **kwds)
-        # some programs use the scan code even if KEYEVENTF_SCANCODE
-        # isn't set in dwFflags, so attempt to map the correct code.
-        if not self.dwFlags & KEYEVENTF_UNICODE:
-            self.wScan = user32.MapVirtualKeyExW(self.wVk,
-                                                 MAPVK_VK_TO_VSC, 0)
+# Actuals Functions
 
-class INPUT(ctypes.Structure):
-    class _INPUT(ctypes.Union):
-        _fields_ = (("ki", KEYBDINPUT),
-                    ("mi", MOUSEINPUT),
-                    ("hi", HARDWAREINPUT))
-    _anonymous_ = ("_input",)
-    _fields_ = (("type",   wintypes.DWORD),
-                ("_input", _INPUT))
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
-LPINPUT = ctypes.POINTER(INPUT)
-
-def _check_count(result, func, args):
-    if result == 0:
-        raise ctypes.WinError(ctypes.get_last_error())
-    return args
-
-user32.SendInput.errcheck = _check_count
-user32.SendInput.argtypes = (wintypes.UINT, # nInputs
-                             LPINPUT,       # pInputs
-                             ctypes.c_int)  # cbSize
-
-# Functions
-
-def PressKey(keyCode):
-    x = INPUT(type=KEYBOARD, ki=KEYBDINPUT(wVk=keyCode))
-    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
-
-def ReleaseKey(keyCode):
-    x = INPUT(type=KEYBOARD, ki=KEYBDINPUT(wVk=keyCode, dwFlags=KEYEVENTF_KEYUP))
-    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
 def Forward():
-    PressKey(W)
-    time.sleep(2)
-    ReleaseKey(W)
+	PressKey(W)
+	time.sleep(0.5)
+	ReleaseKey(W)
 
-if __name__ == "__main__":
-    Forward()
+def Left():
+	PressKey(A)
+	time.sleep(0.01)
+	ReleaseKey(A)
+
+def Right():
+	PressKey(D)
+	time.sleep(0.01)
+	ReleaseKey(D)
+
+def Back():
+	PressKey(S)
+	time.sleep(0.5)
+	ReleaseKey(S)
